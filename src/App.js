b8a52aa1d5bd083d4004d886be955588a1282235
darshann345 <134913449+darshann345@users.noreply.github.com>
2025-06-23 14:28:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -11,35 +11,35 @@ const App = () => {
   };
 
   return (
-    <>
-      <div>
-        <form onSubmit={handleSubmit}>
-          <h1>Full Name Display</h1>
-          <label htmlFor="firstName">
-            First Name:
-            <input
-              id="firstName"
-              value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
-              required
-            />
-            <br />
-          </label>
-          <label htmlFor="lastName">
-            Last Name:
-            <input
-              id="lastName"
-              value={lastName}
-              onChange={(e) => setLastName(e.target.value)}
-              required
-            />
-          </label>
-          <br />
-          <button type="submit">Submit</button>
-        </form>
-        {fullName && <p>Full Name: {fullName}</p>}
-      </div>
-    </>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <h1>Full Name Display</h1>
+        <label htmlFor="firstName">
+          First Name:
+          <input
+            id="firstName"
+            type="text"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            required
+          />
+        </label>
+        <br />
+        <label htmlFor="lastName">
+          Last Name:
+          <input
+            id="lastName"
+            type="text"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+            required
+          />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+      {fullName && <p>Full Name: {fullName}</p>}
+    </div>
   );
 };
 
